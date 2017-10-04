3addad4d60aa4a872294404d683c67861cfc756e
Noah Dayan <noahdayan@gmail.com>
2017-10-04 16:39:01 +0100

    Update recipe-generator.js

--- js/recipe-generator.js
+++ js/recipe-generator.js
@@ -3,7 +3,7 @@
 var pickOne=function(e){
     var n=e[Math.floor(Math.random()*e.length)];return n
 };
-function newRecipe(){
+function generateRecipe(){
     var m=["{{ site.data.recipes.protein | join: '","' }}"],
     h=pickOne(m),
     l=["{{ site.data.recipes.vegetable | join: '","' }}"],
@@ -14,7 +14,7 @@ function newRecipe(){
     t=pickOne(u),
     s=["{{ site.data.recipes.herb | join: '","' }}"],
     n=pickOne(s),
-    e=["{{ site.data.recipes.title | join: '","' }}"],
+    e=["{{ site.data.recipes.titlename | join: '","' }}"],
     i=pickOne(e),
     o=["{{ site.data.recipes.lastname | join: '","' }}"],
     r=pickOne(o),
@@ -23,8 +23,17 @@ function newRecipe(){
     p=["{{ site.data.recipes.seasoning | join: '","' }}"],
     k=pickOne(p),
     a="<h2>"+i+" "+r+"'s "+j+"<br>"+h+", "+c+", "+g+", "+n+", "+k+"<br>and "+t+" Recipe!</h2>";
-    document.getElementById("recipeName").innerHTML=a
+    document.getElementById("generateRecipeName").innerHTML=a
 };
-document.getElementById("recipeButton").onclick=function(){
-    newRecipe()
+document.getElementById("generateRecipeButton").onclick=function(){
+    generateRecipe()
+};
+function findRecipe(){
+    var m=["{{ site.data.recipes.recipename | join: '","' }}"],
+    h=pickOne(m),
+    a="<h2>"+h+"!</h2>";
+    document.getElementById("findRecipeName").innerHTML=a
+};
+document.getElementById("findRecipeButton").onclick=function(){
+    findRecipe()
 };
